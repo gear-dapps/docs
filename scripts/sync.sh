92993aa9e418ecc10ed364cd5f388a5ca1058148
Shamil <shamil@shamil.ru>
2022-05-30 16:16:42 +0300

    Add more programs

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
-REPOS=ping
+REPOS="\
+concert \
+dao \
+dao-light \
+dutch-auction \
+escrow \
+feeds \
+fungible-token \
+gear-lib \
+lottery \
+multitoken \
+non-fungible-token \
+ping \
+"
 
 set -e
 cd "$(dirname "$0")/.."
@@ -8,10 +21,14 @@ cd "$(dirname "$0")/.."
 for REPO in $REPOS
 do
     if [ ! -d $REPO ]; then
-        git clone https://github.com/gear-academy/ping.git
+        git clone https://github.com/gear-academy/$REPO.git
     else
         cd $REPO
+        git reset --hard
         git pull
         cd ..
     fi
 done
+
+rm feeds/Cargo.toml
+rm non-fungible-token/Cargo.toml
